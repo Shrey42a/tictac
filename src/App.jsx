09c259a83092ae55e22821fc0adbe812c60906aa
Shrey kumar <shreykumar55242@gmail.com>
2023-11-05 15:40:44 +0530

    adjusting responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,17 @@ function App() {
 }
   return (
     <>
-      <div className="App">
+      <div className="App py-4">
             {winner && <div className="winner">
                 <div className='winner-text'>{winner}</div>
                 <button onClick={() => resetBoard()}>Reset</button>
             </div>}
             <h2 className="h1 text-4xl lg:text-7xl md:text-6xl sm:text-5xl text-center tracking-widest">Tic Tac Toe</h2>
             <div className="flex space-y-2 lg:space-y-0 lg:flex-row flex-col w-1/2 lg:w-full items-center justify-around">
-              <input onChange={handleChange} value={name} placeholder="Player 1"  className="w-60 lg:py-1 px-4 text-xl font-bold rounded-sm text-center bg-transparent border-2 rounded-md border-gray-900 outline-none" />
-              <input onChange={handleChange2} value={name2} placeholder="Player 2" className="w-60 px-4 lg:py-1 text-xl rounded-sm font-bold text-center bg-transparent border-2 rounded-md border-gray-900 outline-none" />
+              <input onChange={handleChange} value={name} placeholder="Player 1"  className="w-60 lg:py-1 px-4 shadow-inner shadow-zinc-500 text-xl font-bold rounded-md text-center bg-transparent border rounded-md border-gray-900 outline-none" />
+              <input onChange={handleChange2} value={name2} placeholder="Player 2" className="w-60 px-4 lg:py-1 shadow-inner shadow-zinc-500  text-xl rounded-md font-bold text-center bg-transparent border rounded-md border-gray-900 outline-none" />
             </div>
+            <button onClick={resetBoard} className='px-2 my-2 py-1 rounded-lg active:translate-y-1 transition ease-in-out bg-orange-500 shadow-lg shadow-zinc-700'>Reset now</button>
             <Page reset={reset} setReset={setReset} winner={winner} setWinner={setWinner} title={name} title2={name2} />
       </div>
       <Footer />
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer(){
     return(
         <>
-        <footer className="h-max bg-[url(/bg5.jpg)] bg-center flex lg:flex-row flex-col items-center py-2">
+        <footer className="h-max bg-[url(/bg1.jpg)] bg-center flex lg:flex-row flex-col items-center py-2">
     <p>Copyright &#169; 2023, All rights reserverd</p>
     <p>Design and created by Shrey kumar</p>
   </footer>
